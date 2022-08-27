f2e780c67ca9e85f102f02de3de291d2f3013b30
caraje <carlosajenjo@gmail.com>
2022-08-27 02:55:26 +0200

    ordernar redes

--- utils/socialNetworks.js
+++ utils/socialNetworks.js
@@ -1,8 +1,8 @@
 export const socialNetworks = [
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/Caraje_',
-    icon: <i className='fa-brands fa-twitter'></i>,
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/carlos-ajenjo-19662076/',
+    icon: <i className='fa-brands fa-linkedin'></i>,
   },
   {
     name: 'Github',
@@ -10,13 +10,13 @@ export const socialNetworks = [
     icon: <i className='fa-brands fa-github'></i>,
   },
   {
+    name: 'Twitter',
+    url: 'https://twitter.com/Caraje_',
+    icon: <i className='fa-brands fa-twitter'></i>,
+  },
+  {
     name: 'Instagram',
     url: 'https://www.instagram.com/caraje.dev/',
     icon: <i className='fa-brands fa-instagram'></i>,
-  },
-  {
-    name: 'Linkedin',
-    url: 'https://www.linkedin.com/in/carlos-ajenjo-19662076/',
-    icon: <i className='fa-brands fa-linkedin'></i>,
   },
 ]
